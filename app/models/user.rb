49a7dbd8b3d588fb023181788d05c4592b52050a
Jerry <hall.jerry9@gmail.com>
2020-02-07 14:48:33 -0800

    Removed ability to sign up for accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :confirmable, # removed registerable
          :recoverable, :rememberable, :validatable
 
   # Reference to Article model
