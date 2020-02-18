46c259961bb498e91b72ceae9494d14514335ba5
Jerry <hall.jerry9@gmail.com>
2020-02-18 08:36:39 -0800

    added friendly_id functionality to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,11 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true, length: {minimum: 1, maximum: 30}
-  validates :description, presence: true, length: {minimum: 1, maximum: 300}
-  validates :user_id, presence: true
+    # Added FriendlyId functionality:
+    # This allows article ('event') paths to be displayed by title name
+    extend FriendlyId
+    friendly_id :title, use: :slugged
+
+    belongs_to :user
+    validates :title, presence: true, length: {minimum: 1, maximum: 30}
+    validates :description, presence: true, length: {minimum: 1, maximum: 300}
+    validates :user_id, presence: true
 end
